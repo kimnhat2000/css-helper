b7d40f444c8da2dde392f81820605717518b0f2f
ACER <kimnhat2000@gmail.com>
2018-01-24 08:30:10 +0700

    css helper boiler plate

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports =(env)=>{
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('style.css')
+  return {
+    // entry: './src/redux/flashCardRedux.js',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap:true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+      }, {
+        test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+        loader: 'file-loader',
+      }]
+    },
+    plugins:[
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    } 
+  }
+};
+
